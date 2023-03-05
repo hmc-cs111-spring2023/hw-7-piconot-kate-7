3565f889e8e3d2852103ce58a9fdb1c058f47eda
Kate Phillips <47925526+muffinkate@users.noreply.github.com>
2023-03-05 23:02:19 +0000

    working on parser

--- evaluation.md
+++ evaluation.md
@@ -14,6 +14,12 @@ describe_ why _you made the change._
 _Describe each change from your ideal syntax to the syntax you implemented, and
 describe_ why _you made the change._
 
+trying to extract surroundings from not explicit surroundings info - adding a helper function, and then seeing it if it works. 
+
+decided i needed a way to say what state it currently is in. "staying" implies you have the same state, but when "turning" you need to say from which state to the other.
+
+similarly, i needed a way to make sure i knew which direction was blocked. there are a few ways I could probably have approached this, but for now am just adding in a direction
+
 **On a scale of 1–10 (where 10 is "a lot"), how much did you have to change your syntax?**
 
 **On a scale of 1–10 (where 10 is "very difficult"), how difficult was it to map your syntax to the provided API?**
--- src/main/scala/piconot/Parser.scala
+++ src/main/scala/piconot/Parser.scala
@@ -10,9 +10,10 @@ object PiconotParser extends JavaTokenParsers {
 
     // rules
     def rule: Parser[Rule] =
-        "while there is road ahead, drive " ~> dir ~ ("on route " ~> state) ^^ {
-            case "while there is road ahead, drive " ~> d ~ ("on route " ~> s) => Rule(Surroundings())
-        }
+        "while there is road ahead, stay " ~> dir ~ ("on route " ~> state) ^^ {
+            case "while there is road ahead, drive " ~> d ~ ("on route " ~> s) => Rule(state, DriveStraight(d),d,state) }
+        | "when the "  ~> dir  ~ ("road ends, turn off" ~> state ~ ("and turn " ~> dir ~ ("onto route " ~> state))) ^^ {
+            case "when the " ~> d_old ("road ends, turn off" ~> s_old ~ ("and turn " ~> d ~ ("onto route " ~> s_new))) => Rule(s_old, RoadEnds(d_old), d, s_new) }
     
     def dir: Parser[MoveDirection] =
         // parser for returning a MoveDirection
@@ -22,3 +23,15 @@ object PiconotParser extends JavaTokenParsers {
     
     
 }
+
+def DriveStraight(dir: MoveDirection): Surroundings = dir match
+    case North => Surroundings(Open, Anything, Anything, Anything)
+    case East => Surroundings(Anything, Open, Anything, Anything)
+    case West => Surroundings(Anything, Anything, Open, Anything)
+    case South => Surroundings(Anything, Anything, Anything, Open)
+
+def RoadEnds(dir: MoveDirection): Surroundings = dir match
+    case North => Surroundings(Blocked, Anything, Anything, Anything)
+    case East => Surroundings(Anything, Blocked, Anything, Anything)
+    case West => Surroundings(Anything, Anything, Blocked, Anything)
+    case South => Surroundings(Anything, Anything, Anything, Blocked)
