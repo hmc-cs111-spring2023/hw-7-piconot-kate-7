16784745856cc4c4eb06fb7b11e42cba0591960d
Kate Phillips <47925526+muffinkate@users.noreply.github.com>
2023-03-06 01:27:22 +0000

    working on parser

--- evaluation.md
+++ evaluation.md
@@ -16,9 +16,11 @@ describe_ why _you made the change._
 
 trying to extract surroundings from not explicit surroundings info - adding a helper function, and then seeing it if it works. 
 
-decided i needed a way to say what state it currently is in. "staying" implies you have the same state, but when "turning" you need to say from which state to the other.
+decided i needed a way to say what state it currently is in. "staying" implies you have the same state, but when "turning" you need to say from which state to the other. If you don't move, but need to change state, i used the language "get off" and "get on" to 
 
 similarly, i needed a way to make sure i knew which direction was blocked. there are a few ways I could probably have approached this, but for now am just adding in a direction
+
+the last thing i needed to hammer out was how to handle surroundings information when there was two-four different things someone is specifying. I debated whether or not to make that explicit, or if you could leave it out or just say "hey the user isn't allowed to do that in this language" but I decided I'd rather make the language a bit more awkward and instead keep that functionality. To handle this, I just expanded the "when the X road ends" statement, so that if it's blocked the X road has ended, you can implicitly say that a road is open by saying to turn (right now assuming that that error would get cauught later, and not in the design of the language) and then
 
 **On a scale of 1–10 (where 10 is "a lot"), how much did you have to change your syntax?**
 
--- src/main/scala/piconot/Parser.scala
+++ src/main/scala/piconot/Parser.scala
@@ -8,18 +8,29 @@ object PiconotParser extends JavaTokenParsers {
     // parsing interface
     def apply(s: String): ParseResult[Rule] = parseAll(rule, s)
 
-    // rules
+    // rules - need cases for any number of directions and if or not the state changes, and for if you turn or brake
     def rule: Parser[Rule] =
-        "while there is road ahead, stay " ~> dir ~ ("on route " ~> state) ^^ {
-            case "while there is road ahead, drive " ~> d ~ ("on route " ~> s) => Rule(state, DriveStraight(d),d,state) }
-        | "when the "  ~> dir  ~ ("road ends, turn off" ~> state ~ ("and turn " ~> dir ~ ("onto route " ~> state))) ^^ {
-            case "when the " ~> d_old ("road ends, turn off" ~> s_old ~ ("and turn " ~> d ~ ("onto route " ~> s_new))) => Rule(s_old, RoadEnds(d_old), d, s_new) }
+        "while there is road ahead, stay" ~> dir ~ ("on route" ~> state) ^^ {
+            case d ~ s => Rule(state, DriveStraight(d),d,state) } // direction same way that picodrive is moving, same state
+        | "when the"  ~> dir  ~ ("road ends, turn off" ~> state ~ ("and turn" ~> dir ~ ("onto route" ~> state))) ^^ {
+            case d_old ~ s_old ~  d ~  s_new => Rule(s_old, RoadEnds(d_old), d, s_new) } // changing state And direction because ahead direction is blocked
+        | "when the" ~> dir ~ ("road ends, turn" ~> dir ~ ("to stay on route" ~> state)) ^^ {
+            case d_old ~ d_new ~  s => Rule(s, RoadEnds(d_old), d_new, s) } // changing direction but not state
+        | "when the" ~> dir ~ ("road ends," ~> dir ~ ("to stay on route" ~> state)) ^^ {
+            case d_old ~ brake ~ s => Rule(s, RoadEnds(d_old), brake, s) } // not moving, nor changing state
+        | "when the" ~> dir ~ ("road ends," ~> dir ~("to get off route" ~> state ~ ("and get on route" ~> state))) ^^ {
+            case d_old ~ brake ~ s => Rule()
+        }
     
     def dir: Parser[MoveDirection] =
-        // parser for returning a MoveDirection
+        "North" ^^ {case "North" => North}
+        | "East" ^^ {case "East" => East}
+        | "West" ^^ {case "West" => West}
+        | "South" ^^ {case "South" => South}
+        | "brake" ^^ {case "brake" => StayHere}
 
     def state: Parser[State] =
-         // parser for returning a State
+        wholeNumber ^^ {case n => State(n)}
     
     
 }
